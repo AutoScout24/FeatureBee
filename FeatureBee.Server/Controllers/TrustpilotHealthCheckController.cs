d8e26c6e17e8b90a46452a0a27bfb900fa13673b
Bruce Mackenzie Nielsen <bmn@trustpilot.com>
2014-07-21 11:25:43 +0200

    Added our health check controller.

---
+++ FeatureBee.Server/Controllers/TrustpilotHealthCheckController.cs
@@ -0,0 +1,46 @@
+﻿namespace FeatureBee.Server.Controllers
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    public class TrustpilotHealthCheckController : ApiController
+    {
+        private static bool isHealthy = true;
+
+        [HttpGet]
+        [Route("health-check")]
+        public HttpResponseMessage HealthCheck()
+        {
+            if (isHealthy)
+            {
+                return new HttpResponseMessage()
+                {
+                    Content = new StringContent("OK")
+                };
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.ServiceUnavailable)
+            {
+                Content = new StringContent("Deployment in process")
+            };
+        }
+
+        [HttpPost]
+        [Route("fail-health-check")]
+        public HttpResponseMessage FailHealthCheck()
+        {
+            if (!Request.IsLocal())
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            isHealthy = false;
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("OK")
+            };
+        }
+    }
+}
